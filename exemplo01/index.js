162f9b08e97447c1fd0d8b7909fb9163965161a1
Marina Tanaka <huttenloucher@gmail.com>
2023-09-12 12:01:49 -0300

    Exemplo de como usar o node via terminal do VScode

--- exemplo01/index.js
+++ exemplo01/index.js
@@ -5,3 +5,15 @@ let nota2 = 7.2;
 let media = (nota1 + nota2) / 2;
 
 console.log(media);
+
+console.log("---------");
+
+let dados = {
+    nome: "Tanaka",
+    idade: 21 ,
+    signo: "escorpião"
+};
+
+console.log(dados.signo);
+
+console.table(dados);
