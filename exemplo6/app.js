169fc56504203e4b1297722dad7ad14045c9bc6f
Marina Tanaka <huttenloucher@gmail.com>
2023-09-15 10:55:08 -0300

    Update nos exemplos
    rotas

---
+++ exemplo6/app.js
@@ -0,0 +1,30 @@
+// Importação do framework express.
+import express from 'express';
+
+// Guardando o express em uma variável. Ao invés de chamar o express().get, vamos utilizar o app.get; 
+const app = express();
+
+// Configurando a view engine com o EJS
+app.set('view engine', 'ejs');
+
+// Configurando as rotas de acesso:
+    // Raiz = '/'
+    // res.send('Bem-Vindo(a) ao servidor ExpressJS!') - Testando se o framework está funcionando.
+        app.get('/', (req, res) => {
+            res.render('paginas/index')
+        })
+
+        app.get('/sobre', (req, res) => {
+            res.render('paginas/sobre')
+        })
+
+        app.get('/contato', (req, res) => {
+            res.render('paginas/contato')
+        })
+  
+
+
+// Iniciando o servidor e escolhendo a porta de acesso.
+app.listen(3000, () => {
+    console.log('Servidor ExpressJS rodando normalmente.')
+});
